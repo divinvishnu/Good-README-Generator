dfe465dde97d459dda2108aa9f49410d65d50413
Kirill Kuznetsov <yours.kkuznets@gmail.com>
2020-05-09 11:33:40 +1000

    Added badges

--- assets/generateREADME.js
+++ assets/generateREADME.js
@@ -1,22 +1,23 @@
 function generateREADME({ userName, userEmail, projectTitle, projectDescription, projectImgSRC, projectInstall, projectRun, projectTest, projectInfo, projectLicense, avatarURL }) {
-
+  projectTitleDashed = projectTitle.replace(/\s+/g, '-');
   return `
-  [contributors-shield]: https://img.shields.io/github/contributors/${userName}/${projectTitle}.svg?style=flat-square
-  [contributors-url]: https://github.com/${userName}/${projectTitle}/graphs/contributors
-  [forks-shield]: https://img.shields.io/github/forks/${userName}/${projectTitle}.svg?style=flat-square
-  [forks-url]: https://github.com/${userName}/${projectTitle}/network/members
-  [stars-shield]: https://img.shields.io/github/stars/${userName}/${projectTitle}.svg?style=flat-square
-  [stars-url]: https://github.com/${userName}/${projectTitle}/stargazers
-  [issues-shield]: https://img.shields.io/github/issues/${userName}/${projectTitle}.svg?style=flat-square
-  [issues-url]: https://github.com/${userName}/${projectTitle}/issues
-  [license-shield]: https://img.shields.io/github/license/${userName}/${projectTitle}.svg?style=flat-square
-  [license-url]: https://github.com/${userName}/${projectTitle}/blob/master/LICENSE.txt
+  [contributors-shield]: https://img.shields.io/github/contributors/${userName}/${projectTitleDashed}.svg?style=flat-square
+  [contributors-url]: https://github.com/${userName}/${projectTitleDashed}/graphs/contributors
+  [forks-shield]: https://img.shields.io/github/forks/${userName}/${projectTitleDashed}.svg?style=flat-square
+  [forks-url]: https://github.com/${userName}/${projectTitleDashed}/network/members
+  [stars-shield]: https://img.shields.io/github/stars/${userName}/${projectTitleDashed}.svg?style=flat-square
+  [stars-url]: https://github.com/${userName}/${projectTitleDashed}/stargazers
+  [issues-shield]: https://img.shields.io/github/issues/${userName}/${projectTitleDashed}.svg?style=flat-square
+  [issues-url]: https://github.com/${userName}/${projectTitleDashed}/issues
+  [license-shield]: https://img.shields.io/github/license/${userName}/${projectTitleDashed}.svg?style=flat-square
+  [license-url]: https://github.com/${userName}/${projectTitleDashed}/blob/master/LICENSE.txt
 
   [![Contributors][contributors-shield]][contributors-url] [![Forks][forks-shield]][forks-url] [![Stargazers][stars-shield]][stars-url] [![Issues][issues-shield]][issues-url] [![License.txt][license-shield]][license-url]
 
   # ${projectTitle}
 
-  ## ${projectDescription}
+  ## Description
+  ${projectDescription}
 
   ## Table of Contents
   - [Title](#title)
@@ -47,14 +48,16 @@ function generateREADME({ userName, userEmail, projectTitle, projectDescription,
   ${projectInfo}
 
   ## License
-  This project is licensed under the ${projectLicense} License - see the [LICENSE.txt](https://github.com/yours-kkuznets/Wheather-Forecast/blob/master/LICENSE.txt) file for details
+  This project is licensed under the ${projectLicense} License - see the [LICENSE.txt](https://github.com/${userName}/${projectTitleDashed}/blob/master/LICENSE.txt) file for details
 
   ## Contributing
-  Please read [CONTRIBUTING.md](https://github.com/${userName}/${projectTitle}/blob/master/CONTRIBUTING.md) for details on the code of conduct, and the process for submitting pull requests.
+  Please read [CONTRIBUTING.md](https://github.com/${userName}/${projectTitleDashed}/blob/master/CONTRIBUTING.md) for details on the code of conduct, and the process for submitting pull requests.
 
   ## Questions and Feedback
   Please contact me using one of the following:
-  - Github: [<img src="${avatarURL}" height="30" width="30" alt="${userName}"/>](https://gist.github.com/${userName}) 
+  |---|---|
+  - | Github: [${userName}](https://gist.github.com/${userName}) | [<img src="${avatarURL}" height="30" width="30" alt="${userName}"/>](https://gist.github.com/${userName}) |
+  |---|---|
   - Email: ${userEmail}
   `
 }
